552d01cffc4b001f40bbd7a1218584de34f59c57
Rifki Martleo Alfiansyah <rifkimartleoa@student.uns.ac.id>
2025-05-07 16:29:01 +0700

    app.py

--- app.py
+++ app.py
@@ -36,6 +36,15 @@ if option == "Beranda":
         st.balloons()
         st.write("Terima kasih telah mengklik tombol!")
 
+    uploaded_file = st.file_uploader("Pilih gambar untuk diunggah", type=["png", "jpg", "jpeg"])
+
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.success("✅ Gambar berhasil diunggah!")
+        st.image(image, caption="Gambar yang Anda unggah", use_column_width=True)
+    else:
+        st.info("Silakan unggah gambar dengan format .png, .jpg, atau .jpeg.")
+    
 elif option == "Data":
     st.header("Data")
     
